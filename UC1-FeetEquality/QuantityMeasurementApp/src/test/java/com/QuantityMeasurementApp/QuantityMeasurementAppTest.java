2db167e91403faaeaa6168e693baa2077c087430
Aditi181123 <tanuaditi04@gmail.com>
2026-02-18 16:29:48 +0530

    [Aditi Mittal] Add , UC1 , Feet Measurement Equality

---
+++ UC1-FeetEquality/QuantityMeasurementApp/src/test/java/com/QuantityMeasurementApp/QuantityMeasurementAppTest.java
@@ -0,0 +1,40 @@
+package com.QuantityMeasurementApp;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class QuantityMeasurementAppTest {
+	/*
+	 * Test for Feet Measurement Quantity
+	 */
+	
+    @Test
+    public void Test_FeetEquality_SameValue(){
+    	QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(1.0);
+        assertTrue(f1.equals(f2));
+    }
+    @Test
+    public void Test_FeetEquality_DiffValue() {
+    	QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(2.0);
+
+        assertFalse(f1.equals(f2));
+    }
+    @Test
+    public void Test_FeetEquality_NullComp() {
+    	QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        assertFalse(f1.equals(null));
+    }
+    @Test
+    public void Test_FeetEquality_DiffClass() {
+    	QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        assertFalse(f1.equals("1.0"));
+    }
+    @Test 
+    public void Test_FeetEquality_SameRefrence() {
+    	QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        assertTrue(f1.equals(f1));
+    }
+}

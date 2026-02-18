e1ba6814643ea3210d1600f1b94dabd0e7bc36d2
Aditi181123 <tanuaditi04@gmail.com>
2026-02-18 17:24:56 +0530

    [Aditi Mittal] Add, Uc1 Feet Equlity

--- src/test/java/com/QuantityMeasurementApp/QuantityMeasurementAppTest.java
+++ src/test/java/com/QuantityMeasurementApp/QuantityMeasurementAppTest.java
@@ -9,7 +9,7 @@ public class QuantityMeasurementAppTest {
 	 * Test for Feet Measurement Quantity
 	 */
 	
-    @Test
+	@Test
     public void Test_FeetEquality_SameValue(){
     	QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
         QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(1.0);
@@ -35,6 +35,37 @@ public class QuantityMeasurementAppTest {
     @Test 
     public void Test_FeetEquality_SameRefrence() {
     	QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        assertTrue(f1.equals(f1));
+    }
+    /*
+     * Test for Inches Measurement Quantity
+     */
+    @Test
+    public void Test_InchEquality_SameValue(){
+    	QuantityMeasurementApp.Inches i1 = new QuantityMeasurementApp.Inches(1.0);
+        QuantityMeasurementApp.Inches i2 = new QuantityMeasurementApp.Inches(1.0);
+        assertTrue(i1.equals(i2));
+    }
+    @Test
+    public void Test_InchEquality_DiffValue() {
+    	QuantityMeasurementApp.Inches f1 = new QuantityMeasurementApp.Inches(1.0);
+        QuantityMeasurementApp.Inches f2 = new QuantityMeasurementApp.Inches(2.0);
+
+        assertFalse(f1.equals(f2));
+    }
+    @Test
+    public void Test_InchEquality_NullComp() {
+    	QuantityMeasurementApp.Inches f1 = new QuantityMeasurementApp.Inches(1.0);
+        assertFalse(f1.equals(null));
+    }
+    @Test
+    public void Test_InchEquality_DiffClass() {
+    	QuantityMeasurementApp.Inches f1 = new QuantityMeasurementApp.Inches(1.0);
+        assertFalse(f1.equals("1.0"));
+    }
+    @Test 
+    public void Test_InchEquality_SameRefrence() {
+    	QuantityMeasurementApp.Inches f1 = new QuantityMeasurementApp.Inches(1.0);
         assertTrue(f1.equals(f1));
     }
 }
